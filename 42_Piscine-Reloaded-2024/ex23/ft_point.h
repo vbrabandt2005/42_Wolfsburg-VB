5e87e81080e01f8e789a1a9249b85dbe103bebcd
vbrabandt2005 <vbrabandt@proton.me>
2024-06-07 00:59:57 +0200

    idk what to do with my life

--- 42_Piscine-Reloaded-2024/ex23/ft_point.h
+++ 42_Piscine-Reloaded-2024/ex23/ft_point.h
@@ -3,11 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbraband <vbraband@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vbrabandt <vbrabandt@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 09:26:54 by vbraband          #+#    #+#             */
-/*   Updated: 2024/06/06 17:29:27 by vbraband         ###   ########.fr       */
+/*   Updated: 2024/06/07 00:57:49 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+#endif
 
