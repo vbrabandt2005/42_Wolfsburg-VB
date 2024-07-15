b0b4938b42f708470ebaf65926040125321b8751
vbrabandt2005 <vbrabandt@proton.me>
2024-07-15 16:32:52 +0200

    get_line_stiff
    I'm just being lazy lol, me feels sleepy today

---
+++ Get_Next_Line-2024/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbraband <vbraband@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 14:32:28 by vbraband          #+#    #+#             */
+/*   Updated: 2024/07/15 15:52:54 by vbraband         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+
+
+#endif
