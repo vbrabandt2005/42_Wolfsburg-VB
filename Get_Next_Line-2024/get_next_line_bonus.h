2a4b7298200f3151f64015251a49fdfa29461fd4
vbrabandt2005 <vbrabandt@proton.me>
2024-07-31 19:11:30 +0200

    some code idk

--- Get_Next_Line-2024/get_next_line_bonus.h
+++ Get_Next_Line-2024/get_next_line_bonus.h
@@ -1,13 +1,12 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <limits.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
-# include <limits.h>
 # include <stdio.h>
-# include <fcntl.h>
-# include <string.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
@@ -15,7 +14,7 @@
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *string, int searchedChar );
+char	*ft_strchr(const char *string, int searchedChar);
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t elementCount, size_t elementSize);
--- Get_Next_Line-2024/get_next_line_utils_bonus.c
+++ Get_Next_Line-2024/get_next_line_utils_bonus.c
@@ -1,4 +1,4 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
