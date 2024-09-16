3b5fa746565196c0ea63e378f72f2e785b41a298
Vincent Brabandt <vbraband@student.42wolfsburg.de>
2024-09-16 13:33:25 +0200

    minor start with push_swap :(

---
+++ Circle-02_August2024/Push_Swap-September2024/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbraband <vbraband@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 13:26:58 by vbraband          #+#    #+#             */
+/*   Updated: 2024/09/16 13:29:57 by vbraband         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+
+#endif
