591449f3cd3b476df8d57ab3ee0405377f0df2fb
Vincent Brabandt <vbraband@student.42wolfsburg.de>
2024-09-27 14:06:14 +0200

    Still not working
    :(

--- Circle-02_August2024/MiniTalk-October2024/srcs/client.c
+++ Circle-02_August2024/MiniTalk-October2024/srcs/client.c
@@ -6,49 +6,48 @@
 /*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 14:49:41 by vbraband          #+#    #+#             */
-/*   Updated: 2024/09/27 13:19:10 by vbrabandt        ###   ########.fr       */
+/*   Updated: 2024/09/27 13:52:02 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/MiniTalk.h"
 
-void    send_char(int pid, unsigned char c)
+void	send_char(int pid, unsigned char c)
 {
-    int i;
-    unsigned char temp;
+	int				i;
+	unsigned char	temp;
 
-    i = 8;
-    while (i > 0)
-    {
-        i--;
-        temp = c >> i;
-        if (temp & 1)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-    }
+	i = 8;
+	while (i > 0)
+	{
+		i--;
+		temp = c >> i;
+		if (temp & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+	}
 }
 
-
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-    pid_t server_pid;
-    const char *message;
-    int i;
+	pid_t server_pid;
+	const char *message;
+	int i;
 
-   if (argc != 3)
-    {
-        ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-        return (1);
-    }
-    server_pid = ft_atoi(argv[1]);
-    message = argv[2];
-    i = 0;
-    while (message[i])
-    {
-        send_char(server_pid, message[i]);
-        i++;
-    }
-    send_char(server_pid, END_TRANSMISSION);
-    return (0);
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
+		return (1);
+	}
+	server_pid = ft_atoi(argv[1]);
+	message = argv[2];
+	i = 0;
+	while (message[i] != '\0')
+	{
+		send_char(server_pid, message[i]);
+		i++;
+	}
+	send_char(server_pid, END_TRANSMISSION);
+	return (0);
 }
--- Circle-02_August2024/MiniTalk-October2024/srcs/server.c
+++ Circle-02_August2024/MiniTalk-October2024/srcs/server.c
@@ -6,41 +6,41 @@
 /*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 14:49:39 by vbraband          #+#    #+#             */
-/*   Updated: 2024/09/27 13:27:57 by vbrabandt        ###   ########.fr       */
+/*   Updated: 2024/09/27 13:52:13 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/MiniTalk.h"
 
-void    handler(int sig)
+void	handler(int sig)
 {
-    static unsigned char    current_char;
-    static int              current_bit;
+	static unsigned char	current_char = 0;
+	static int				current_bit = 0;
 
-    current_char |= (sig == SIGUSR1);
-    current_bit++;
-
-    if (current_bit == 8)
-    {
-        if (current_char == END_TRANSMISSION)
-        {
-            ft_printf("\n");
-        }
-        else
-            ft_printf("%c", current_char);
-        current_char = 0;
-        current_bit = 0;
-    }
-    else
-        current_char <<= 1;
+	if (sig == SIGUSR1)
+		current_char |= (1 << (7 - current_bit));
+	current_bit++;
+	if (current_bit == 8)
+	{
+		if (current_char == END_TRANSMISSION)
+		{
+			ft_printf("\n");
+		}
+		else
+		{
+			ft_printf("%c", current_char);
+		}
+		current_char = 0;
+		current_bit = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    ft_printf("%d\n", getpid());
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
-    while (1)
-        pause();
-    return (0);
+	ft_printf("%d\n", getpid());
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
+	return (0);
 }
