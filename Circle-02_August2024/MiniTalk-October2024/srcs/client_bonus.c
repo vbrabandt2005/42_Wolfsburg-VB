f93076ed685f007fd2593e1abe0900df2f79b1cb
Vincent Brabandt <vbraband@student.42wolfsburg.de>
2024-09-27 14:35:28 +0200

    me cry in fixes tries

--- Circle-02_August2024/MiniTalk-October2024/srcs/client_bonus.c
+++ Circle-02_August2024/MiniTalk-October2024/srcs/client_bonus.c
@@ -6,7 +6,48 @@
 /*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 13:04:39 by vbrabandt         #+#    #+#             */
-/*   Updated: 2024/09/27 13:04:41 by vbrabandt        ###   ########.fr       */
+/*   Updated: 2024/09/27 14:26:35 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/MiniTalk.h"
+
+void	send_char(int pid, unsigned char c)
+{
+	int				i;
+	unsigned char	temp;
+
+	i = 8;
+	while (i > 0)
+	{
+		i--;
+		temp = c >> i;
+		if (temp & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	pid_t server_pid;
+	const char *message;
+	int i;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
+		return (1);
+	}
+	server_pid = ft_atoi(argv[1]);
+	message = argv[2];
+	i = 0;
+	while (message[i] != '\0')
+	{
+		send_char(server_pid, message[i]);
+		i++;
+	}
+	send_char(server_pid, END_TRANSMISSION);
+	return (0);
+}
---
+++ Circle-02_August2024/MiniTalk-October2024/srcs/server_bonus.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 14:13:15 by vbrabandt         #+#    #+#             */
+/*   Updated: 2024/09/27 14:18:48 by vbrabandt        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/MiniTalk.h"
+#include <signal.h>
+#include <unistd.h>
+
+void	handle_signal(int signal, siginfo_t *info, void *context)
+{
+	static unsigned char	current_char;
+	static int				bit_index;
+
+	(void)context;
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == END_TRANSMISSION)
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
+	if (signal == SIGUSR1)
+		kill(info->si_pid, SIGUSR1);
+	else if (signal == SIGUSR2)
+		kill(info->si_pid, SIGUSR2);
+}
+
+/**
+ * @brief    Prints its program's PID and calls the signal handlers.
+ */
+int	main(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	printf("%d\n", getpid());
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
