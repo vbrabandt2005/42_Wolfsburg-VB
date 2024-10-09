f13ecf686d722785c9509198ae34e1259e230d5b
Vincent Brabandt <vbraband@student.42wolfsburg.de>
2024-10-09 15:17:13 +0200

    just copied over normal to bonus (WIP)
    FOR BONUS!!!!

--- Circle-02_August2024/MiniTalk-October2024/srcs/server_bonus.c
+++ Circle-02_August2024/MiniTalk-October2024/srcs/server_bonus.c
@@ -6,7 +6,61 @@
 /*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 14:13:15 by vbrabandt         #+#    #+#             */
-/*   Updated: 2024/10/02 13:21:58 by vbrabandt        ###   ########.fr       */
+/*   Updated: 2024/10/09 15:16:11 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/MiniTalk.h"
+
+void	rec(int n, int pid, int *clientpid)
+{
+	int			shift;
+	static int	i = 7;
+	static char	c;
+
+	if (i == -1 || pid != *clientpid)
+	{
+		i = 7;
+		c = 0;
+		*clientpid = pid;
+	}
+	shift = 1 << (i);
+	if (n != 0)
+		c = (c | shift);
+	i--;
+	if (i == -1)
+		write(1, &c, 1);
+}
+
+void	server(void)
+{
+	int	pid;
+
+	pid = getpid();
+	ft_printf("Your lovely PID: %d\n", pid);
+}
+
+void	sig_handler(int sig, siginfo_t *info, void *ptr)
+{
+	static int	clientpid = -1;
+
+	(void)ptr;
+	if (sig == SIGUSR1)
+		rec(1, info->si_pid, &clientpid);
+	else
+		rec(0, info->si_pid, &clientpid);
+}
+
+int	main(void)
+{
+	struct sigaction	act;
+
+	act.sa_flags = 0;
+	act.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &act, 0);
+	sigaction(SIGUSR2, &act, 0);
+	server();
+	while (1)
+		pause();
+	return (0);
+}
