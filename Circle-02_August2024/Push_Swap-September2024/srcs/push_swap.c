8d378e64fec380bda93a0e60fc6523b24863fb08
Vincent Brabandt <vbraband@student.42wolfsburg.de>
2024-10-11 20:50:05 +0200

    idk lol

--- Circle-02_August2024/Push_Swap-September2024/srcs/push_swap.c
+++ Circle-02_August2024/Push_Swap-September2024/srcs/push_swap.c
@@ -3,11 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbraband <vbraband@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 13:25:51 by vbraband          #+#    #+#             */
-/*   Updated: 2024/09/16 15:01:17 by vbraband         ###   ########.fr       */
+/*   Updated: 2024/10/11 20:49:53 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int push_swap(void)
+{
+    printf("Push_Swap CRY!\n");
+    return (0);
+}
