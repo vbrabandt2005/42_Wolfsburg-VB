6873fe62bae3b592e2b74908fc881be471352ac7
Vincent Wutthisit Brabandt <vbrabandt@proton.me>
2024-10-23 13:30:05 +0200

    Update push_swap.h

--- Circle-02_August2024/Push_Swap-September2024/includes/push_swap.h
+++ Circle-02_August2024/Push_Swap-September2024/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:46:41 by vbrabandt         #+#    #+#             */
-/*   Updated: 2024/10/23 12:46:41 by vbrabandt        ###   ########.fr       */
+/*   Updated: 2024/10/23 13:19:18 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,42 @@
 # define PUSH_SWAP_H
 
 # include "../lib/Libft_Modified-2024/srcs/libft.h"
-# include <limits.h>    //To define MIN and MAX macros
-# include <stdbool.h>   //To use bool flags, i. e to print or not to print
+# include <limits.h>
 # include <signal.h>
+# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <unistd.h>
 
-# define PRINT true     //To print the operations
-# define SILENT false   //To not print the operations
+# define PRINT true
+# define SILENT false
 
 typedef struct s_stack_node
 {
-    int                     num;            // The number to sort
-    int                     index;          // The number´s position in the stack
-    int                     push_cost;      // How many commands in total
-    bool                    above_median;   // Used to calculate 'push_cost
-    bool                    cheapest;       // The node that is the cheapes to do commands
-    struct s_stack_node    *target_node;   // The target node of a node in the opposite stack
-    struct s_stack_node    *next;          // a pointer to the next node
-    struct s_stack_node    *prev;          // a pointer to the previous node
-}   t_stack_node;                          // the "shortened name"
+	int					num;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
 
-/* --- Error Handling --- */
+	struct s_stack_node	*target_node;
 
-/* --- Stack Inititation --- */
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
 
-/* --- Node Initiation --- */
-
-/* --- Stack Utils --- */
-
-/* --- Commands --- */
-void    sa(t_stack_node **a, bool print_mode);
-void    sb(t_stack_node **b, bool print_mode);
-void    ss(t_stack_node **a, t_stack_node **b, bool print_mode);
-void    ra(t_stack_node **a, bool print_mode);
-void    rb(t_stack_node **b, bool print_mode);
-void    rr(t_stack_node **a, t_stack_node **b, bool print_mode);
-void    rra(t_stack_node **a, bool print_mode);
-void    rrb(t_stack_node **b, bool print_mode);
-void    rrr(t_stack_node **a, t_stack_node **b, bool print_mode);
-void    pa(t_stack_node **a, t_stack_node **b, bool print_mode);
-void    pb(t_stack_node **a, t_stack_node **b, bool print_mode);
-
-/* --- Algorithm --- */
-
+void					sa(t_stack_node **a, bool print_mode);
+void					sb(t_stack_node **b, bool print_mode);
+void					ss(t_stack_node **a, t_stack_node **b, bool print_mode);
+void					ra(t_stack_node **a, bool print_mode);
+void					rb(t_stack_node **b, bool print_mode);
+void					rr(t_stack_node **a, t_stack_node **b, bool print_mode);
+void					rra(t_stack_node **a, bool print_mode);
+void					rrb(t_stack_node **b, bool print_mode);
+void					rrr(t_stack_node **a, t_stack_node **b,
+							bool print_mode);
+void					pa(t_stack_node **a, t_stack_node **b, bool print_mode);
+void					pb(t_stack_node **a, t_stack_node **b, bool print_mode);
 
 #endif
