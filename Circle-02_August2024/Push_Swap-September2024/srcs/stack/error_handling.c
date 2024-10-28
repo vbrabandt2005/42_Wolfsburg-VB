1db0566274b1761170d85facb0c81de263ba14fb
Vincent Brabandt <vbraband@student.42wolfsburg.de>
2024-10-28 14:43:02 +0100

    Update error_handling.c

--- Circle-02_August2024/Push_Swap-September2024/srcs/stack/error_handling.c
+++ Circle-02_August2024/Push_Swap-September2024/srcs/stack/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: vbrabandt <vbrabandt@proton.me>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:32:34 by vbrabandt         #+#    #+#             */
-/*   Updated: 2024/10/28 14:17:34 by vbrabandt        ###   ########.fr       */
+/*   Updated: 2024/10/28 14:42:57 by vbrabandt        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,43 @@
 
 int	error_syntax(char *str_n)
 {
-    if (!(*str_n == '+' || *str_n == '-' || (*str_n >= '0' && *str_n <= '9')))
-        return (1);
-    if ((*str_n == '+' || *str_n == '-') && !(str_n[1] >= '0' && str_n[1] <= '9'))
-        return (1);
-    while (*++str_n)
-        if (!(*str_n >= '0' && *str_n <= '9'))
-            return (1);
-    return (0);
+	if (!(*str_n == '+' || *str_n == '-' || (*str_n >= '0' && *str_n <= '9')))
+		return (1);
+	if ((*str_n == '+' || *str_n == '-') && !(str_n[1] >= '0'
+			&& str_n[1] <= '9'))
+		return (1);
+	while (*++str_n)
+		if (!(*str_n >= '0' && *str_n <= '9'))
+			return (1);
+	return (0);
 }
 
 int	error_duplicate(t_stack_node *a, int n)
 {
-    while (a)
-    {
-        if (a->num == n)
-            return (1);
-        a = a->next;
-    }
-    return (0);
+	while (a)
+	{
+		if (a->num == n)
+			return (1);
+		a = a->next;
+	}
+	return (0);
 }
 
 void	free_stack(t_stack_node **stack)
 {
-    t_stack_node	*tmp;
+	t_stack_node	*tmp;
 
-    while (*stack)
-    {
-        tmp = (*stack)->next;
-        free(*stack);
-        *stack = tmp;
-    }
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
 }
 
 void	free_errors(t_stack_node **a)
 {
-    free_stack(a);
-    ft_printf("Error\n");
-    exit(1);
+	free_stack(a);
+	ft_printf("Error\n");
+	exit(1);
 }
